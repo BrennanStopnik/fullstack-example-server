1519c61152ef7c704e4a2800d628900eda4ea68b
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-11-16 10:48:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require("cors")
+const cors = require("cors");
 require("dotenv").config();
 
 var indexRouter = require('./routes/index');
